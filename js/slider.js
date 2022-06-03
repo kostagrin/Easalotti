efea3a35e4a7883e461c3b08e525088fe618bf55
Konstantin Grinev <konstantingrinev7@gmail.com>
2022-06-03 11:48:17 +0300

    change images

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,6 @@
-'use strict';
-const images = [
- `./img/hero-1.jpg`,
- `./img/hero-2.jpg`,
- `./img/hero-3.jpg`,
-];
+"use strict";
 
-const bgImages = Array.from(document.querySelectorAll(".bg-img"));
+const homeBgImages = Array.from(document.querySelectorAll(".home__bg-img"));
 const dots = document.querySelector(".dots");
 const dot = Array.from(document.querySelectorAll(".dot"));
 
@@ -13,55 +8,55 @@ let slideIndex = 0;
 let slideInterval;
 
 function resetDots() {
- dot.forEach((dot) => {
-   dot.classList.remove("active");
- });
+  dot.forEach((dot) => {
+    dot.classList.remove("active");
+  });
 }
 
 function resetAll() {
- resetDots();
- bgImages.forEach((img) => img.classList.remove("active"));
+  resetDots();
+  homeBgImages.forEach((img) => img.classList.remove("active"));
 }
 
 const updateSlideIndex = function () {
- slideIndex++;
- if (slideIndex >= bgImages.length) slideIndex = 0;
+  slideIndex++;
+  if (slideIndex >= homeBgImages.length) slideIndex = 0;
 };
 
 function changeSlide(i) {
- bgImages[i].classList.add("active");
+  homeBgImages[i].classList.add("active");
 }
 
 function changeDot(i) {
- dot[i].classList.add("active");
+  dot[i].classList.add("active");
 }
 
 function updateBg(i) {
- resetAll();
- updateSlideIndex();
- changeSlide(i);
- changeDot(i);
+  resetAll();
+  updateSlideIndex();
+  changeSlide(i);
+  changeDot(i);
 }
 
 function startSlider() {
- slideInterval = setInterval(() => {
-   updateBg(slideIndex)  
- }, 5000);
+  slideInterval = setInterval(() => {
+    updateBg(slideIndex);
+  }, 5000);
 }
 
 function stopSlider() {
- clearInterval(slideInterval);
+  clearInterval(slideInterval);
 }
 
 dots.addEventListener("click", (e) => {
- const clickedDot = e.target.closest(".dot");
- if (!clickedDot || clickedDot.classList.contains("active")) return;
- resetAll();
- stopSlider();
- slideIndex = clickedDot.dataset.bgDot;
- changeDot(slideIndex);
- changeSlide(slideIndex);
- startSlider();
+  const clickedDot = e.target.closest(".dot");
+  if (!clickedDot || clickedDot.classList.contains("active")) return;
+  resetAll();
+  stopSlider();
+  slideIndex = clickedDot.dataset.bgDot;
+  changeDot(slideIndex);
+  changeSlide(slideIndex);
+  startSlider();
 });
 
 startSlider();
